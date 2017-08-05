fda42768a77329145f9e1020936d2101305c15f6
Antoine Bache <antoine.bache@epitech.eu>
2017-08-05 20:02:35 +0200

    improved SSE detection

--- kernel/src/arch/x86/sse.s
+++ kernel/src/arch/x86/sse.s
@@ -1,13 +1,19 @@
 [bits 32]
 section .text
 global check_sse
+global check_sse2
+global check_sse3
+global check_ssse3
+global check_sse41
+global check_sse42
+global check_sse4A
 
 ;; Returns 1 if SSE is present AND activated, else 0
 check_sse:
 mov   eax, 0x1
 cpuid
 test  edx, 1 << 25
-jz    .noSSE
+jz    noSSE
 mov   eax, cr0
 and   ax, 0xFFFB	;; clear coprocessor emulation CR0.EM
 or    ax, 0x2			;; set coprocessor monitoring  CR0.MP
@@ -18,6 +24,60 @@ mov   cr4, eax
 mov   eax, 1
 ret
 
-.noSSE:
+;; Check is SSE2 is supported
+check_sse2:
+mov   eax, 0x1
+cpuid
+test  edx, 1 << 26
+jz    noSSE
+mov   eax, 1
+ret
+
+;; Check is SSE3 is supported
+check_sse3:
+mov   eax, 0x1
+cpuid
+test  ecx, 1 << 0
+jz    noSSE
+mov   eax, 1
+ret
+
+;; Check is SSSE3 is supported
+check_ssse3:
+mov   eax, 0x1
+cpuid
+test  ecx, 1 << 9
+jz    noSSE
+mov   eax, 1
+ret
+
+;; Check is SSE4.1 is supported
+check_sse41:
+mov   eax, 0x1
+cpuid
+test  ecx, 1 << 19
+jz    noSSE
+mov   eax, 1
+ret
+
+;; Check is SSE4.2 is supported
+check_sse42:
+mov   eax, 0x1
+cpuid
+test  ecx, 1 << 20
+jz    noSSE
+mov   eax, 1
+ret
+
+;; Check is SSE4A is supported
+check_sse4A:
+mov   eax, 0x1
+cpuid
+test  ecx, 1 << 6
+jz    noSSE
+mov   eax, 1
+ret
+
+noSSE:
 mov   eax, 0
 ret
--- kernel/src/arch/x86/sse_c.c
+++ kernel/src/arch/x86/sse_c.c
@@ -2,9 +2,46 @@
 #include <kernel/logger.h>
 
 extern int check_sse();
+extern int check_sse2();
+extern int check_sse3();
+extern int check_ssse3();
+extern int check_sse41();
+extern int check_sse42();
+extern int check_sse4A();
 
 void init_sse(void) {
+  // Check SSE and activate it if supported
   if (check_sse()) {
     LOG(LOG_INFO, "SSE activated.\r\n");
+  }
+
+  // Check SSE2 support
+  if (check_sse2()) {
+    LOG(LOG_INFO, "SSE2 supported\r\n");
+  }
+
+  // Check SSE3 support
+  if (check_sse3()) {
+    LOG(LOG_INFO, "SSE3 supported\r\n");
+  }
+
+  // Check SSSE3 support
+  if (check_ssse3()) {
+    LOG(LOG_INFO, "SSSE3 supported\r\n");
+  }
+
+  // Check SSE4.1 support
+  if (check_sse41()) {
+    LOG(LOG_INFO, "SSE4.1 supported\r\n");
+  }
+
+  // Check SSE4.2 support
+  if (check_sse42()) {
+    LOG(LOG_INFO, "SSE4.2 supported\r\n");
+  }
+
+  // Check SSE4A support
+  if (check_sse4A()) {
+    LOG(LOG_INFO, "SSE4A supported\r\n");
   }
 }
